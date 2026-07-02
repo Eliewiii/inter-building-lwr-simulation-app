28ed5c1454a221c69ddb7f661e22528c4ee5acb3
Eliewiii <elie.medioniwiii@gmail.com>
2026-07-02 12:11:26 +0300

    cleanup linting

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -2,5 +2,6 @@
 
 from app.services.auth import get_current_user_id
 from app.services.orchestrator import schedule_pipeline
+from app.services.state_manager import update_manifest
 
-__all__ = ["get_current_user_id", "schedule_pipeline"]
+__all__ = ["get_current_user_id", "schedule_pipeline", "update_manifest"]
